d028f2883c39291d5a7a8b7db9595aa697a7d6dc
Dinuka7L <dinoo.walker7@gmail.com>
2025-06-29 15:31:47 +0530

    Update GPA in education data
    Changed the GPA value for the current undergraduate entry in the educationData array from 3.52/4.0 to 3.54/4.0 to reflect the latest academic record.

--- src/components/QualificationsSection.tsx
+++ src/components/QualificationsSection.tsx
@@ -23,7 +23,7 @@ const educationData: Education[] = [
     location: 'Perth, Australia',
     year: '2022 - Present',
     description: 'Final year undergrad in Cybersecurity with a focus on mastering skills on offensive and defensive security. Shaped by practical experiences and real-world applications, allowing myself to apply theories into practical implementations.',
-    grade: 'GPA: 3.52/4.0',
+    grade: 'GPA: 3.54/4.0',
     logo: 'https://www.ecu.edu.au/__data/assets/image/0004/1100389/ecu-logo.png',
     gradientColors: ['#00ffc9','#2563eb', '#1e3a8a','#4b00b4'] //Colors
   },
